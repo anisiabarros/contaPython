9819a1bbe3eef0c321398bd504608c6a6a7d22e5
anisiabarros <anisia.barros@gmail.com>
2023-07-07 21:39:23 -0300

    adicionando getter e setters

--- conta.py
+++ conta.py
@@ -11,8 +11,31 @@ class Conta:
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.__saldo+= valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.__saldo-=valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, origem, destino):
+        origem.saca(valor)
+        destino.deposita(valor)
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def numero(self):
+        return self.__numero
 
